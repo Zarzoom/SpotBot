1f65c6c15d768ca41d971997851b9a4610e9c289
zarzoom <SophiaSeely@outlook.com>
2023-08-23 19:37:17 -0500

    SpotBot start. created new project

---
+++ spotBackBotUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace spotBackBotUnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
